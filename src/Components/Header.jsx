d5b5c12cd830d12736b572c79a2c79d53a94edc6
Decode <amitkumar696945@gmail.com>
2024-01-11 23:26:19 +0530

    header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -61,7 +61,7 @@ function Header() {
         </div>
         </div>
         <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
-        className='w-[40px] rounded-full'/>
+        className='w-[40px] ml-12 rounded-full'/>
     </div>
   )
 }
