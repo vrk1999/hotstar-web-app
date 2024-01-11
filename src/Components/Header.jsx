04bb32d606889b1ae76e30397080f808f8db4d63
Decode <amitkumar696945@gmail.com>
2024-01-11 22:40:05 +0530

    icons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
 import logo from '../assets/images/hotstar.png'
-import {HiHome, HiStar,HiPlayCircle,HiTv} from 'react-icons/hi2'
+import { HiHome,
+    HiMagnifyingGlass,
+    HiStar,
+    HiPlayCircle,
+    HiTv } from "react-icons/hi2";
+import { HiPlus,HiDotsVertical } from "react-icons/hi";
+
 const Header = () => {
+    const menu=[
+        {
+            name:'HOME',
+            icon:HiHome
+        },
+        {
+            name:'SEARCH',
+            icon:HiMagnifyingGlass
+        },
+        {
+            name:'WATCH LIST',
+            icon:HiPlus
+        },
+        {
+            name:'ORIGINALS',
+            icon:HiStar
+        },
+        {
+            name:'MOVIES',
+            icon:HiPlayCircle
+        },
+        {
+            name:'SERIES',
+            icon:HiTv
+        }
+    ]
   return (
     <div>
         <img src={logo} className='w-[80px] object-cover md:w-[115px]'  alt="logo" />
