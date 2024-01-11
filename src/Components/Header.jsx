e680ffdac1e3836628d755c98b7d408e648ca061
Decode <amitkumar696945@gmail.com>
2024-01-11 22:33:30 +0530

    react-icons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import logo from '../assets/images/hotstar.png'
+import {HiHome, HiStar,HiPlayCircle,HiTv} from 'react-icons/hi2'
 const Header = () => {
   return (
     <div>
-        <img src={logo} className='w-[80px] object-cover'  alt="logo" />
+        <img src={logo} className='w-[80px] object-cover md:w-[115px]'  alt="logo" />
     </div>
   )
 }
