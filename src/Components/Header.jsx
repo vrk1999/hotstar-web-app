9dfe4833c8e24ef769ae0b3959d9700ba38e7564
Decode <amitkumar696945@gmail.com>
2024-01-11 23:18:04 +0530

    header completed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import logo from '../assets/images/hotstar.png'
+import React, { useState } from 'react'
+import logo from '../assets/images/logo.png'
 import { HiHome,
     HiMagnifyingGlass,
     HiStar,
@@ -7,8 +7,8 @@ import { HiHome,
     HiTv } from "react-icons/hi2";
 import { HiPlus,HiDotsVertical } from "react-icons/hi";
 import HeaderItem from './HeaderItem';
-
-const Header = () => {
+function Header() {
+    const [toggle,setToggle]=useState(false);
     const menu=[
         {
             name:'HOME',
@@ -36,16 +36,32 @@ const Header = () => {
         }
     ]
   return (
-    <div className='flex items-center gap-8'>
-        <div>
-        <img src={logo} className='w-[80px] object-cover md:w-[115px]'  alt="logo" />
+    <div className='flex items-center justify-between p-5'>
+        <div className='flex  gap-8 items-center'>
+        <img src={logo} className='w-[80px] 
+        md:w-[115px] object-cover' />
+        <div className='hidden md:flex gap-8'>
         {menu.map((item)=>(
-        <HeaderItem name={item.name} Icon= {item.icon}/>
+            <HeaderItem name={item.name} Icon={item.icon} />
         ))}
+        </div>
+          <div className='flex md:hidden gap-5'>
+        {menu.map((item,index)=>index<3&&(
+            <HeaderItem name={''} Icon={item.icon} />
+        ))}
+         <div className='md:hidden' onClick={()=>setToggle(!toggle)}>       
+            <HeaderItem name={''} Icon={HiDotsVertical} />
+           {toggle? <div className='absolute mt-3 bg-[#121212] 
+            border-[1px] border-gray-700 p-3 px-5 py-4'>
+            {menu.map((item,index)=>index>2&&(
+            <HeaderItem name={item.name} Icon={item.icon} />
+            ))}
+            </div>:null}
+            </div> 
+        </div>
         </div>
         <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
         className='w-[40px] rounded-full'/>
-
     </div>
   )
 }
