3e188cec0b0fe99cd93a93c00761b77a2109de52
Decode <amitkumar696945@gmail.com>
2024-01-12 20:48:30 +0530

    logo arranged

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -61,7 +61,7 @@ function Header() {
         </div>
         </div>
         <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
-        className='w-[40px] ml-12 rounded-full'/>
+        className='w-[40px] ml-80 rounded-full'/>
     </div>
   )
 }
