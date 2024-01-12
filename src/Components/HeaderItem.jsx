4e7129c5e30366d0ce7438f638661c3ac1aa2ed8
Decode <amitkumar696945@gmail.com>
2024-01-12 21:00:54 +0530

    responsive header

--- src/Components/HeaderItem.jsx
+++ src/Components/HeaderItem.jsx
@@ -5,7 +5,7 @@ const HeaderItem = ({name,Icon}) => {
     <div className='text-white flex items-center gap-3
      text-[18px] font-semibold cursor-pointer hover:underline underline-offset-8 '>
         <Icon/>
-        <h2>{name}</h2>
+        <h2 className=''>{name}</h2>
     </div>
   )
 }
