360718c41823ec0c96fd480a3d6a7664ae3c6f3c
Decode <amitkumar696945@gmail.com>
2024-01-13 13:58:28 +0530

    map function

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import { useEffect, useState } from 'react'
+import GlobalApi from './Services/GlobalApi'
 const Slider = () => {
+    const [movieList, setMovieList] = useState([]);
+
+    useEffect(()=>{
+    getTrendingMovies();
+    },[])
+    
+    const getTrendingMovies = ()=>{
+    
+        GlobalApi.getTrendingVideos.then(resp=>{
+          console.log(resp.data.results);
+          setMovieList(resp.data.results)
+        }) 
+    }
   return (
-    <div>Slider</div>
+    <div>
+        {movieList}
+    </div>
   )
 }
 
