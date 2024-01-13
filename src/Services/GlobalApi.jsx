d63ff1ee128ce7da54d431f27d24fb4cf4ad65ab
Decode <amitkumar696945@gmail.com>
2024-01-13 12:41:15 +0530

    created api for services

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -10,4 +10,9 @@ const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=2
 const getTrendingVideos=axios.get(movieBaseUrl+
     "/trending/all/day?api_key="+api_key);
     const getMovieByGenreId=(id)=>
-    axios.get(movieByGenreBaseURL+"&with_genres="+id)
+    axios.get(movieByGenreBaseURL+"&with_genres="+id);
+
+    export default{
+        getTrendingVideos,
+        getMovieByGenreId
+    }
