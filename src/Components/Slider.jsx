81f206fcb220042e4f09b05cfa7cd494b45691db
Decode <amitkumar696945@gmail.com>
2024-01-13 19:12:39 +0530

    talwind-css no-scrollbar

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -17,11 +17,11 @@ const Slider = () => {
     });
   };
   return (
-    <div>
+    <div className="flex overflow-x-auto w-full px-16 py-4">
       {movieList.map((item, index) => (
         <img
           src={IMAGE_BASE_URL + item.backdrop_path}
-          className="min-w-full h-[310px]"
+          className="min-w-full h-[310px] object-cover object-left-top mr-5 rounded-md "
           alt="."
         />
       ))}
