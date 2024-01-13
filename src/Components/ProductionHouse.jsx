c719c26e2d3902408edc070c01b62e19102ddcfe
Decode <amitkumar696945@gmail.com>
2024-01-13 23:31:18 +0530

    list item

--- src/Components/ProductionHouse.jsx
+++ src/Components/ProductionHouse.jsx
@@ -1,13 +1,52 @@
 import React from 'react'
-import disney from './../assets/disney.png'
-import marvel from './../assets/marvel.png'
-import national from './../assets/nationalG.png'
-import pixar from './../assets/pixar.png'
-import starwar from './../assets/starwar.png'
+import disney from './../assets/images/disney.png'
+import marvel from './../assets/images/marvel.png'
+import national from './../assets/images/nationalG.png'
+import pixar from './../assets/images/pixar.png'
+import starwar from './../assets/images/starwar.png'
+
+import starwarV from './../assets/Videos/star-wars.mp4'
+import disneyV from './../assets/Videos/disney.mp4'
+import marvelV from './../assets/Videos/marvel.mp4'
+import nationalGeoV from './../assets/Videos/national-geographic.mp4'
+import pixarV from './../assets/Videos/pixar.mp4'
+
+
 
 const ProductionHouse = () => {
+    const productionHouseList=[
+        {
+            id:1,
+            image:disney,
+            video:disneyV
+        },
+        {
+            id:2,
+            image:pixar,
+            video:pixarV
+        },
+        {
+            id:3,
+            image:marvel,
+            video:marvelV
+        },
+        {
+            id:4,
+            image:starwar,
+            video:starwarV
+        },
+        {
+            id:5,
+            image:national,
+            video:nationalGeoV
+        },
+
+    ]
+
   return (
-    <div>ProductionHouse</div>
+    <div>
+
+    </div>
   )
 }
 
