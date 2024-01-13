43b72f78dc2cefa3944290c521b70fec6fd98156
Decode <amitkumar696945@gmail.com>
2024-01-13 12:48:17 +0530

    slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 
+import { useEffect } from 'react'
 import Header from './Components/Header'
+import Slider from './Components/Slider'
+import GlobalApi from './Services/GlobalApi'
 
 function App() {
 
+useEffect(()=>{
 
+},[])
+
+const getTrendingMovies = ()=>{
+
+    GlobalApi.getTrendingVideos.then(resp=>{
+      console.log(resp);
+    }) 
+}
   return (
     <>
       <Header/>
+      <Slider/>
     </>
   )
 }
