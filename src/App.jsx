27d79cd5e86a6bc907d9eba84011d21bde50a759
Decode <amitkumar696945@gmail.com>
2024-01-13 22:41:25 +0530

    added videos and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 
 import Header from './Components/Header'
+import ProductionHouse from './Components/ProductionHouse'
 import Slider from './Components/Slider'
 
 
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header/>
       <Slider/>
+      <ProductionHouse/>
     </>
   )
 }
---
+++ src/Components/ProductionHouse.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import disney from './../assets/disney.png'
+import marvel from './../assets/marvel.png'
+import national from './../assets/nationalG.png'
+import pixar from './../assets/pixar.png'
+import starwar from './../assets/starwar.png'
+
+const ProductionHouse = () => {
+  return (
+    <div>ProductionHouse</div>
+  )
+}
+
+export default ProductionHouse
