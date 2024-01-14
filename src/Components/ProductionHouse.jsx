12594a21fcf479d828f89eee0e308e903617a61a
Decode <amitkumar696945@gmail.com>
2024-01-14 09:55:35 +0530

    productionhouse completed

--- src/Components/ProductionHouse.jsx
+++ src/Components/ProductionHouse.jsx
@@ -47,10 +47,12 @@ const ProductionHouse = () => {
     <div className='flex gap-2 md:gap-5 p-2 px-5  md:px-16'>
       {productionHouseList.map((item ,index)=>( 
       <div className='border-[2px] border-gray-600
-      rounded-lg hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer'>
-            <img src={item.image} className='w-full ' />
-            <video src={item.video} autoPlay loop playsInline muted className='absolute z-0 top-0
-            rounded-md opacity-0 hover:opacity-50'></video>
+      rounded-lg hover:scale-110 transition-all duration-300 ease-in-out 
+      cursor-pointer shadow-xl shadow-gray-800'>
+             <video src={item.video} autoPlay loop playsInline muted className='absolute z-0 top-0
+            rounded-md opacity-0 hover:opacity-50'></video> 
+            <img src={item.image} className='w-full z-[1] opacity-100 ' />
+          
         </div>
         ))}
     </div>
