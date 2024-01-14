b63cc54d15276e596c2725318082ffe1b2eaa8d9
Decode <amitkumar696945@gmail.com>
2024-01-14 09:41:43 +0530

    video implemented

--- src/Components/ProductionHouse.jsx
+++ src/Components/ProductionHouse.jsx
@@ -49,6 +49,8 @@ const ProductionHouse = () => {
       <div className='border-[2px] border-gray-600
       rounded-lg hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer'>
             <img src={item.image} className='w-full ' />
+            <video src={item.video} autoPlay loop playsInline muted className='absolute z-0 top-0
+            rounded-md opacity-0 hover:opacity-50'></video>
         </div>
         ))}
     </div>
