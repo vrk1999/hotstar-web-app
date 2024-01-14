dde2255825fb85a31be2003b3475e2e592e7aabc
Decode <amitkumar696945@gmail.com>
2024-01-14 11:54:38 +0530

    updated api responses

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const movieBaseUrl="https://api.themoviedb.org/3"
-const api_key='2ec0d66f5bdf1dd12eefa0723f1479cf'
+const api_key='226fa11333b277fbfc18b9649fc449fc'
 
-const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf';
+const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=226fa11333b277fbfc18b9649fc449fc';
 
-//https://api.themoviedb.org/3/trending/all/day?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf
+//https://api.themoviedb.org/3/trending/all/day?api_key=226fa11333b277fbfc18b9649fc449fc
 const getTrendingVideos=axios.get(movieBaseUrl+
     "/trending/all/day?api_key="+api_key);
     const getMovieByGenreId=(id)=>
